8967cf6dbb617debd0e92873cbddba54e17eaf2a
Moses-Morris <mosesmorrisdev@gmail.com>
2023-04-25 11:07:52 -0700

    A response status code.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//A script that display the status code of a GET request.
+
+const request = require('request');
+const Request_url = process.argv[2];
+request(Request_url, (err, res, body) => {
+  if (err) console.error(err);
+  console.log('code:', res.statusCode);
+});
+
