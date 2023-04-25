bb2b0981c8a3c58ae22eb88dd965a9b507b026e0
Moses-Morris <mosesmorrisdev@gmail.com>
2023-04-25 12:11:38 -0700

    A script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,18 +3,19 @@
 // A script that computes the number of tasks completed by user id
 
 const request = require('request');
-const url = process.argv[2];
-request(url, (err, res, body) => {
-  if (err) console.error(err);
-  const tasks = JSON.parse(body);
-  const dict = {};
-  for (let i = 0; i < tasks.length; i++) {
-    if (!dict[tasks[i].userId]) {
-      dict[tasks[i].userId] = 0;
+const address = process.argv[2];
+
+request(address, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let results = {};
+    for (let td of JSON.parse(body)) {
+      if (td.completed) {
+        if (results[td['userId']] === undefined) { results[td['userId']] = 0; }
+        results[td['userId']] += 1;
+      }
     }
-    if (tasks[i].completed === true) {
-      dict[tasks[i].userId] += 1;
-    }
+    console.log(results);
   }
-  console.log(dict);
 });
