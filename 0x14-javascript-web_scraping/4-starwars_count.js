055bc770cdb0b5dae52260841bfccc0905f90996
Moses-Morris <mosesmorrisdev@gmail.com>
2023-04-25 11:41:13 -0700

    Fix : count the number of times a chaarcter appears.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,23 @@
 #!/usr/bin/node
-const request = require('request');
-const address = process.argv[2];
+// prints the number of movies where the character Wedge Antilles is present
 
-request(address, function (error, response, body) {
+const args = process.argv;
+let reqURL = args[2];
+let request = require('request');
+request(reqURL, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.log('error:', error); // Print the error if one occurred
   } else {
-    let results = JSON.parse(body).results;
+    let jso = JSON.parse(body);
+    let results = jso['results'];
     let count = 0;
-    for (let i in results) {
-      for (let chr of results[i].characters) {
-        if (chr.search('/18/') > 0) { count += 1; }
+    for (let i = 0; i < results.length; i++) {
+      let chars = (results[i]['characters']);
+      for (let j = 0; j < chars.length; j++) {
+        let check18 = chars[j].endsWith('18/');
+        if (check18) {
+          count++;
+        }
       }
     }
     console.log(count);
