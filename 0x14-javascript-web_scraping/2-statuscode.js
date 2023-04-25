7265c95b880c0379b43236d2541b805e249e20d2
Moses-Morris <mosesmorrisdev@gmail.com>
2023-04-25 12:02:33 -0700

    Get response code.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-// A script that display the status code of a GET request.
+
+// printing the status code of a GET request
 
 const request = require('request');
-const Request_url = process.argv[2];
-request(Request_url, (err, res, body) => {
+const url = process.argv[2];
+request(url, (err, res, body) => {
   if (err) console.error(err);
   console.log('code:', res.statusCode);
 });
